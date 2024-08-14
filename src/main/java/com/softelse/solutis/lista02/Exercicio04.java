35679f88f71500cfa65946368113994e92cb3be4
renatof4rias <renatof4rias@gmail.com>
2024-08-13 22:20:41 -0300

    feat: Lista 1, Questão 4

---
+++ src/main/java/com/softelse/solutis/lista02/Exercicio04.java
@@ -0,0 +1,15 @@
+package com.softelse.solutis.lista02;
+
+import java.util.Scanner;
+
+public class Exercicio04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite a distância em milhas: ");
+        double milhas = sc.nextDouble();
+        double quilometros = milhas * 1.609;
+        System.out.println(milhas + " milhas equivalem a " + quilometros + " quilômetros.");
+        sc.close();
+    }
+}
