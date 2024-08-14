2d5307307807dd02047fa428519e4da7681f2f81
renatof4rias <renatof4rias@gmail.com>
2024-08-13 22:21:19 -0300

    feat: Lista 1, Questão 9

---
+++ src/main/java/com/softelse/solutis/lista02/Exercicio09.java
@@ -0,0 +1,17 @@
+package com.softelse.solutis.lista02;
+
+import java.util.Scanner;
+
+public class Exercicio09 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o raio do círculo: ");
+        double raio = sc.nextDouble();
+        double area = Math.PI * Math.pow(raio, 2);
+        double areaArredondada = Math.round(area);
+        System.out.println("A área do círculo com raio " + raio + " é: " + areaArredondada);
+
+        sc.close();
+    }
+}
