e88163e3614a37f54b0312581cbb50329a06f517
renatof4rias <renatof4rias@gmail.com>
2024-08-13 21:29:51 -0300

    feat: Lista 0, Questão 12

--- src/main/java/com/softelse/solutis/lista01/PrimeiraLista.java
+++ src/main/java/com/softelse/solutis/lista01/PrimeiraLista.java
@@ -72,14 +72,16 @@ Para qualquer outro código: XXX ? Diversos.
             System.out.println("Produto: Diversos");
         }
 
-
-
-
-
-
-
-
-
+/*
+12. Imprima o resultado da divisão por 2 de todos os múltiplos de 3, entre 1 e 100, usando os tipos de dados int e double .
+ */
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0) {
+                int resultadoInt = i / 2;
+                double resultadoDouble = (double) i / 2;
+                System.out.println("Número: " + i + "  Divisão por 2: " + resultadoInt + "Divisão por 2: " + resultadoDouble);
+            }
+        }
 
 
     sc.close();
