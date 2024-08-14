7d3f025f90becbbcd8cf8cead40aa5f0686c7a40
renatof4rias <renatof4rias@gmail.com>
2024-08-13 22:20:49 -0300

    feat: Lista 1, Questão 5

---
+++ src/main/java/com/softelse/solutis/lista02/Exercicio05.java
@@ -0,0 +1,28 @@
+package com.softelse.solutis.lista02;
+
+import java.util.Scanner;
+
+public class Exercicio05 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] diasDaSemana = {
+                "Segunda-feira",
+                "Terça-feira",
+                "Quarta-feira",
+                "Quinta-feira",
+                "Sexta-feira",
+                "Sábado",
+                "Domingo"
+        };
+
+        System.out.print("Digite um número de 1 a 7: ");
+        int numero = sc.nextInt();
+        if (numero >= 1 && numero <= 7) {
+            System.out.println("O dia correspondente é: " + diasDaSemana[numero - 1]);
+        } else {
+            System.out.println("Número inválido! Por favor, digite um número entre 1 e 7.");
+        }
+        sc.close();
+    }
+}
