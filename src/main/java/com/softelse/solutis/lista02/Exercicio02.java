351fa89b6b76edb424c1b3677d4a26f99c124d63
renatof4rias <renatof4rias@gmail.com>
2024-08-13 22:20:26 -0300

    feat: Lista 1, Questão 2

---
+++ src/main/java/com/softelse/solutis/lista02/Exercicio02.java
@@ -0,0 +1,18 @@
+package com.softelse.solutis.lista02;
+
+import java.util.Scanner;
+
+public class Exercicio02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite o primeiro valor: ");
+        int valor1 = sc.nextInt();
+        System.out.print("Digite o segundo valor: ");
+        int valor2 = sc.nextInt();
+        int maior = (valor1 > valor2) ? valor1 : valor2;
+
+        System.out.println("O maior valor é: " + maior);
+
+        sc.close();
+    }
+}
