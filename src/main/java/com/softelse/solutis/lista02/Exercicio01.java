1cd15e72ed62f770d06c7659b562e357c7145494
renatof4rias <renatof4rias@gmail.com>
2024-08-13 22:20:19 -0300

    feat: Lista 1, Questão

---
+++ src/main/java/com/softelse/solutis/lista02/Exercicio01.java
@@ -0,0 +1,12 @@
+package com.softelse.solutis.lista02;
+
+public class Exercicio01 {
+    public static void main(String[] args) {
+
+        int ii = 5;
+        int j = ii;
+        j = 10;
+        System.out.println("i + j = " + j + ii);
+
+    }
+}
