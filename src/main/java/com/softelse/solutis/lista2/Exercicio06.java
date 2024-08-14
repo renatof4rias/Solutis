b7b26c4b7a3a7f8500af7e6db93b4caf64405311
renatof4rias <renatof4rias@gmail.com>
2024-08-14 20:48:37 -0300

    feat: Lista 2, Exercicio 6

---
+++ src/main/java/com/softelse/solutis/lista2/Exercicio06.java
@@ -0,0 +1,10 @@
+package com.softelse.solutis.lista2;
+
+public class Exercicio06 {
+    public static void main(String[] args) {
+        double raio = 5.0;
+        double area = Math.PI * Math.pow(raio, 2);
+
+        System.out.println("A área do círculo é: " + area + " metros quadrados.");
+    }
+}
