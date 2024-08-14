ceee9a9eb2e1e5482320267f3f3018344f3f7492
renatof4rias <renatof4rias@gmail.com>
2024-08-13 21:31:15 -0300

    feat: Lista 0, Questão 13

--- src/main/java/com/softelse/solutis/lista01/PrimeiraLista.java
+++ src/main/java/com/softelse/solutis/lista01/PrimeiraLista.java
@@ -84,6 +84,19 @@ Para qualquer outro código: XXX ? Diversos.
         }
 
 
+
+
+/*
+13. Escreva uma classe que imprima todas as possibilidades de que no lançamento de dois dados tenhamos o valor 7 como resultado da soma dos valores de cada dado.
+ */
+
+        for (int dado1 = 1; dado1 <= 6; dado1++) {
+            for (int dado2 = 1; dado2 <= 6; dado2++) {
+                if (dado1 + dado2 == 7) {
+                    System.out.println("Dado 1: " + dado1 + " Dado 2: " + dado2);
+                }
+            }
+        }
     sc.close();
     }
 }
