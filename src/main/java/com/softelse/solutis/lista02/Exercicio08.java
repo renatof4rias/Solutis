966c5cceab4454bc3532372adbfbd287e819c2fb
renatof4rias <renatof4rias@gmail.com>
2024-08-13 22:21:11 -0300

    feat: Lista 1, Questão 8

---
+++ src/main/java/com/softelse/solutis/lista02/Exercicio08.java
@@ -0,0 +1,34 @@
+package com.softelse.solutis.lista02;
+
+import java.util.Scanner;
+
+public class Exercicio08 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite uma palavra: ");
+        String palavra = sc.nextLine();
+        palavra = palavra.replaceAll("\\s+", "").toLowerCase();
+
+        if (isPalindromo(palavra)) {
+            System.out.println("A palavra '" + palavra + "' é um palíndromo.");
+        } else {
+            System.out.println("A palavra '" + palavra + "' não é um palíndromo.");
+        }
+
+        sc.close();
+    }
+
+    public static boolean isPalindromo(String palavra) {
+        int inicio = 0;
+        int fim = palavra.length() - 1;
+        while (inicio < fim) {
+            if (palavra.charAt(inicio) != palavra.charAt(fim)) {
+                return false;
+            }
+            inicio++;
+            fim--;
+        }
+        return true;
+    }
+}
