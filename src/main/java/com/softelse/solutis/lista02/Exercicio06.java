b5d95ab34285cab3522c09aad4e6c7e7bc8c03ce
renatof4rias <renatof4rias@gmail.com>
2024-08-13 22:20:57 -0300

    feat: Lista 1, Questão 6

---
+++ src/main/java/com/softelse/solutis/lista02/Exercicio06.java
@@ -0,0 +1,17 @@
+package com.softelse.solutis.lista02;
+
+public class Exercicio06 {
+    public static void main(String[] args) {
+
+        int produtoInt = 1;
+        float produtoFloat = 1.0f;
+        for (int i = 15; i <= 30; i++) {
+            if (i % 2 != 0) {
+                produtoInt *= i;
+                produtoFloat *= i;
+            }
+        }
+        System.out.println("Produto dos números ímpares de 15 a 30 (int): " + produtoInt);
+        System.out.println("Produto dos números ímpares de 15 a 30 (float): " + produtoFloat);
+    }
+}
