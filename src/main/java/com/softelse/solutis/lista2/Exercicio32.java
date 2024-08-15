816285e5bdaf01c26e6a088886e847881e79e00d
renatof4rias <renatof4rias@gmail.com>
2024-08-14 22:06:06 -0300

    feat: Lista 2, Exercicio 32

---
+++ src/main/java/com/softelse/solutis/lista2/Exercicio32.java
@@ -0,0 +1,9 @@
+package com.softelse.solutis.lista2;
+
+public class Exercicio32 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 500; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
