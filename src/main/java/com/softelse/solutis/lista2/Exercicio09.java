e3dd814330e2bb6df2376e092bbac8410acbf975
renatof4rias <renatof4rias@gmail.com>
2024-08-14 22:06:12 -0300

    feat: Lista 2, Exercicio 33

---
+++ src/main/java/com/softelse/solutis/lista2/Exercicio09.java
@@ -0,0 +1,20 @@
+package com.softelse.solutis.lista2;
+
+import java.util.Scanner;
+
+public class Exercicio09 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Informe sua idade: ");
+        int idade = sc.nextInt();
+
+        if (idade >= 18) {
+            System.out.println("Você é maior de idade.");
+        } else {
+            System.out.println("Você é menor de idade.");
+        }
+
+        sc.close();
+    }
+}
---
+++ src/main/java/com/softelse/solutis/lista2/Exercicio33.java
@@ -0,0 +1,9 @@
+package com.softelse.solutis.lista2;
+
+public class Exercicio33 {
+    public static void main(String[] args) {
+        for (int i = 10; i <= 200; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
